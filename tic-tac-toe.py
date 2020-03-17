bf7360c36b67acc91c9e424e729578f37f1d3bcf
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-03-17 19:21:00 +0100

    added get_move() and mark()

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,8 @@
+import os
+
+
 def init_board():
-    #Returns an empty 3-by-3 board (with zeros)."""
+    """Returns an empty 3-by-3 board (with zeros)."""
 
     board = [
         [0, 0, 0,],
@@ -10,9 +13,27 @@ def init_board():
 
 
 def get_move(board, player):
-    #Returns the coordinates of a valid move for player on board."""
-    
-    row, col = 0, 0
+    """Returns the coordinates of a valid move for player on board."""
+    input_is_not_valid = True
+    rows = ["A", "B", "C"]
+    columns = ["1", "2", "3"]
+    while input_is_not_valid:
+        user_input = input("Please enter coordinates player " + str(player) + ": ").upper()
+        print()
+        if len(user_input) > 2:  # validate input length
+            print("Please enter valid coordinates player " + str(player) + " (e.g. A1, B3) !\n")
+        elif not user_input[0].isalpha() or not user_input[1].isdecimal():  # validate if input is alpha and decimal
+            print("Please enter valid coordinates player " + str(player) + " (e.g. A1, B3) !\n")
+        elif not user_input[0] in rows or not user_input[1] in columns:  # validate if input is in board borders
+            print("Please enter valid coordinates player " + str(player) + " (e.g. A1, B3) !\n")
+        else:
+            row_index = rows.index(user_input[0])
+            column_index = columns.index(user_input[1])
+            if board[row_index][column_index] != 0:  # validate if input is already on board
+                print("Please enter valid coordinates player " + str(player) + " (e.g. A1, B3) !\n")
+            else:
+                input_is_not_valid = False  # input is OK
+    row, col = row_index, column_index
     return row, col
 
 
@@ -24,7 +45,7 @@ def get_ai_move(board, player):
 
 def mark(board, player, row, col):
     """Marks the element at row & col on the board for player."""
-    pass
+    board[row][col] = player
 
 
 def has_won(board, player):
@@ -48,6 +69,7 @@ def print_result(winner):
 
 
 def tictactoe_game(mode='HUMAN-HUMAN'):
+    os.system("clear")
     board = init_board()
 
     # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
@@ -64,4 +86,5 @@ def main_menu():
 
 
 if __name__ == '__main__':
+
     main_menu()
